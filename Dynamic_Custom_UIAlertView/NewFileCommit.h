b2b73b652906dbb3efd5823f5492d8fdf04913a8
laurenceyou <laurenceyou@gmail.com>
2015-02-21 13:14:20 +0800

    add new file
    thrid commit

---
+++ Dynamic_Custom_UIAlertView/NewFileCommit.h
@@ -0,0 +1,13 @@
+//
+//  NewFileCommit.h
+//  Dynamic_Custom_UIAlertView
+//
+//  Created by youjian on 15-2-21.
+//  Copyright (c) 2015年 www.code.tutsplus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewFileCommit : NSObject
+
+@end
---
+++ Dynamic_Custom_UIAlertView/NewFileCommit.m
@@ -0,0 +1,13 @@
+//
+//  NewFileCommit.m
+//  Dynamic_Custom_UIAlertView
+//
+//  Created by youjian on 15-2-21.
+//  Copyright (c) 2015年 www.code.tutsplus. All rights reserved.
+//
+
+#import "NewFileCommit.h"
+
+@implementation NewFileCommit
+
+@end
