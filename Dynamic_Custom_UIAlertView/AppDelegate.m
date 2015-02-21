1f2bf1fd0d54ad31f32c6bfc487865e23d95c1fd
laurenceyou <laurenceyou@gmail.com>
2015-02-21 13:08:58 +0800

    second commit

--- Dynamic_Custom_UIAlertView/AppDelegate.m
+++ Dynamic_Custom_UIAlertView/AppDelegate.m
@@ -17,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //second commit
+    NSLog(@"seconde commit");
+    
     return YES;
 }
 
